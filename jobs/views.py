b965742e4b85e9b941b7a0d4968a2c1070a31f4a
Bonface Mwicwiri <85786694+softwre6@users.noreply.github.com>
2021-06-17 11:23:06 +0300

    Delete jobs directory

--- jobs/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from jobs.models import MTOJob
-
-
-def home(request):
-    context = {'jobs': MTOJob.objects.all(), }
-    return render(request, 'jobs/index.html', context)
