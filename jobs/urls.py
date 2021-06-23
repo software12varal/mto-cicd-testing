dc769d1906d6a0a313b68c5d2d49ef116602884b
raj-ponmani <software7@varaluae.com>
2021-06-23 10:03:49 +0530

    PR 23/06 admin dashboard and view profile created

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-
-from .views import home,mtoadminsignup,add_job, alljobs
+from users.decorators import varal_admin_required
+from .views import home,mto_admin_signup,admin_dashboard,admin_profile,mto_bank,microtask_page,mal_requirement,add_job, alljobs
 
 urlpatterns = [
     path('', home, name='home'),
-    path('admin-registration/', mtoadminsignup, name='adm-reg'),
+    path('admin-registration/', mto_admin_signup, name='admin_register'),
+    path('admin_dashboard', varal_admin_required(admin_dashboard), name='adminDashboard'),
+    path('mto_bank', varal_admin_required(mto_bank), name='mto_bank'),
+    path('microtask', varal_admin_required(microtask_page), name='microtask'),
+    path('requirements', varal_admin_required(mal_requirement), name='requirements'),
+    path('admin_profile', varal_admin_required(admin_profile), name='admin_profile'),
     path('addjobs/', add_job, name='add_job'),
     path('alljobs/',alljobs,name='alljobs'),
 
--- jobs/views.py
+++ jobs/views.py
@@ -4,23 +4,31 @@ from .forms import MTOAdminSignUpForm
 from django.contrib import messages
 from jobs.forms import JobsForm
 
+from jobs.models import MTOJob, MicroTask, MTOAdminUser
+from .forms import MTOAdminSignUpForm, AdminUpdateProfileForm
+
 def home(request):
     context = {'jobs': MTOJob.objects.all(), }
     return render(request, 'jobs/index.html', context)
 
 
-def mtoadminsignup(request):
+def mto_admin_signup(request):
     if request.method == 'POST':
         f = MTOAdminSignUpForm(request.POST)
         if f.is_valid():
+            instance = f.save(commit=False)
+            instance.is_admin = True
+            instance.is_active = True
+            instance.is_mto = False
+            instance.save()
             f.save()
-            return redirect('/') # Redirect to Dashboard Page
+            return redirect('/admin-login')  # Redirect to Dashboard Page
         else:
             return render(request, 'jobs/admin-register.html', {'form': f})
     context = {
-            'jobs': MTOJob.objects.all(),
-            'form' : MTOAdminSignUpForm()
-             }
+        'jobs': MTOJob.objects.all(),
+        'form': MTOAdminSignUpForm()
+    }
     return render(request, 'jobs/admin-register.html', context)
      
 
@@ -40,4 +48,39 @@ def add_job(request):
 def alljobs(request):
     data = Jobs.objects.all().order_by('-id')
     return render(request, 'jobs/jobs.html', {'data': data})
-     
+
+
+def admin_dashboard(request):
+    print(request.user)
+    return render(request, 'jobs/admin_dashboard.html')
+
+
+def mto_bank(request):
+    return render(request, 'jobs/MTOBank.html')
+
+
+def microtask_page(request):
+    context = {'jobs': MicroTask.objects.all(), }
+    return render(request, 'jobs/Microtask.html', context)
+
+
+def mal_requirement(request):
+    context = {'jobs': MicroTask.objects.all(), }
+    return render(request, 'jobs/MAL_requirement.html', context)
+
+
+def admin_profile(request):
+    data = {}
+    form = AdminUpdateProfileForm( instance=request.user.mtoadminuser)
+    if request.method == "POST":
+        form = AdminUpdateProfileForm(request.POST, instance=request.user.mtoadminuser)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your Profile has been updated!")
+            #return redirect(request, 'jobs/admin_profile.html')
+        else:
+            messages.info(request, "sorry profile is not updated!")
+            #return redirect(request, 'jobs/admin_profile.html')
+    data['form'] = form
+    return render(request, 'jobs/admin_profile.html',data)
+
